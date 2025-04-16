fa170b2f07c444da4ad9e9d7502822924f4aae6d
SpreadZhao <spreadzhao@outlook.com>
2025-04-16 22:58:45 +0800

    default size

--- composeApp/src/desktopMain/kotlin/com/spread/lightdelivery/main.kt
+++ composeApp/src/desktopMain/kotlin/com/spread/lightdelivery/main.kt
@@ -1,8 +1,12 @@
 package com.spread.lightdelivery
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.spread.lightdelivery.ui.MainScreen
 import com.spread.lightdelivery.ui.theme.LightDeliverTheme
 
@@ -10,6 +14,7 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "LightDelivery",
+        state = rememberWindowState(size = DpSize(1500.dp, 1000.dp))
     ) {
         LightDeliverTheme {
             MainScreen(Modifier)
