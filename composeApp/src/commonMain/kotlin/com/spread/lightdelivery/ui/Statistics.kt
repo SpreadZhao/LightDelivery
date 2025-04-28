1ae92d2a0369c34a80a20fcc61f14a30f4cacbee
SpreadZhao <spreadzhao@outlook.com>
2025-04-29 01:19:42 +0800

    simplify statistics

--- composeApp/src/commonMain/kotlin/com/spread/lightdelivery/ui/Statistics.kt
+++ composeApp/src/commonMain/kotlin/com/spread/lightdelivery/ui/Statistics.kt
@@ -32,12 +32,14 @@ import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -79,7 +81,7 @@ fun StatisticsDialog(onDismissRequest: () -> Unit) {
         ) {
             Box(modifier = Modifier.size(650.dp)) {
 
-                val pagerState = rememberPagerState(pageCount = { 4 })
+                val pagerState = rememberPagerState(pageCount = { 3 })
                 val scope = rememberCoroutineScope()
                 val config = currYearAndMonth.run {
                     remember {
@@ -105,7 +107,7 @@ fun StatisticsDialog(onDismissRequest: () -> Unit) {
                         state = pagerState,
                         config = config.value
                     )
-                    val pageNames = listOf("选择日期", "选择客户", "选择产品", "查看结果")
+                    val pageNames = listOf("选择日期", "选择客户", "查看结果")
                     Spacer(modifier = Modifier.weight(1f))
                     SingleChoiceSegmentedButtonRow(
                         modifier = Modifier.padding(bottom = 10.dp).wrapContentSize().align(
@@ -139,9 +141,15 @@ fun StatisticsDialog(onDismissRequest: () -> Unit) {
                     )
                 }
 
-                if (pagerState.currentPage == 3 && config.value.valid) {
+                if (pagerState.currentPage == 2 && config.value.valid) {
                     FilledIconButton(
                         onClick = {
+                            DeliverOperator.saveStatistics(
+                                customerName = config.value.result.customerName,
+                                year = config.value.year,
+                                month = config.value.month + 1,
+                                priceMap = config.value.priceMap
+                            )
                         },
                         modifier = Modifier.size(40.dp).align(Alignment.TopStart)
                             .padding(start = 5.dp, top = 5.dp)
@@ -164,8 +172,7 @@ fun StatisticsPager(modifier: Modifier, state: PagerState, config: StatisticsCon
         when (pageIndex) {
             0 -> DatePickPage(config)
             1 -> CustomerPickPage(config)
-            2 -> ItemPickPage(config)
-            3 -> ResultPage(config.result)
+            2 -> ResultPage(config)
         }
     }
 
@@ -174,30 +181,31 @@ fun StatisticsPager(modifier: Modifier, state: PagerState, config: StatisticsCon
 data class StatisticsConfig(
     var year: Int = 0,
     var month: Int = 0,
-    val customers: MutableList<Pair<String, Boolean>> = mutableStateListOf(),
-    val items: MutableList<Pair<String, Boolean>> = mutableStateListOf()
+    var customerName: String = "",
+    val priceMap: SnapshotStateMap<Int, Double> = mutableStateMapOf<Int, Double>()
+//    val items: MutableList<Pair<String, Boolean>> = mutableStateListOf()
 ) {
 
     data class Result(
         val year: Int,
         val month: Int,
-        val customers: List<String>,
-        val items: List<String>
+        val customerName: String
+//        val items: List<String>
     )
 
     val result: Result
         get() = Result(
             year = year,
             month = month,
-            customers = customers.filter { it.second }.map { it.first },
-            items = items.filter { it.second }.map { it.first }
+            customerName = customerName
+//            items = items.filter { it.second }.map { it.first }
         )
 
     val valid: Boolean
         get() = year > 0
                 && month in Calendar.JANUARY..Calendar.DECEMBER
-                && customers.all { it.second }
-                && items.all { it.second }
+                && customerName.isNotBlank()
+//                && items.all { it.second }
 
 }
 
@@ -220,92 +228,75 @@ fun DatePickPage(config: StatisticsConfig) {
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun CustomerPickPage(config: StatisticsConfig) {
-    val displayCustomers = remember { config.customers }
+    val displayCustomers = remember {
+        mutableStateListOf<Pair<String, Boolean>>()
+    }
     LaunchedEffect("only once") {
         SheetViewModel.customerNamesInSheet.forEach { customer ->
             val target = displayCustomers.find { it.first == customer }
             if (target == null) {
-                // customer not show, make it unselected
-                displayCustomers.add(Pair(customer, false))
+                if (config.customerName == customer) {
+                    displayCustomers.add(Pair(customer, true))
+                } else {
+                    // customer not show, make it unselected
+                    displayCustomers.add(Pair(customer, false))
+                }
             }
         }
     }
     FlowRow(modifier = Modifier.padding(10.dp).wrapContentSize()) {
-        SelectAllChip(
-            selected = displayCustomers.all { it.second },
-            onSelectChange = { select ->
-                for (i in displayCustomers.indices) {
-                    displayCustomers[i] = Pair(displayCustomers[i].first, select)
-                }
-            }
-        )
         for (customer in displayCustomers) {
-            CustomerChip(name = customer.first, selected = customer.second, onSelectChange = {
-                val index = displayCustomers.indexOf(customer)
-                displayCustomers[index] = Pair(customer.first, !customer.second)
-            })
+            SelectChip(
+                name = customer.first,
+                selected = customer.second,
+                onSelectChange = { select ->
+                    // select this, unselect others
+                    for (i in displayCustomers.indices) {
+                        if (displayCustomers[i].first == customer.first) {
+                            displayCustomers[i] = displayCustomers[i].copy(second = select)
+                            config.customerName = if (select) customer.first else ""
+                        } else {
+                            displayCustomers[i] = displayCustomers[i].copy(second = false)
+                        }
+                    }
+                })
         }
     }
 }
 
-@Composable
-fun CustomerChip(name: String, selected: Boolean, onSelectChange: (Boolean) -> Unit) {
-    FilterChip(
-        modifier = Modifier.padding(5.dp),
-        onClick = {
-            onSelectChange(!selected)
-        },
-        label = {
-            Text(name)
-        },
-        selected = selected,
-        leadingIcon = if (selected) {
-            {
-                Icon(
-                    imageVector = Icons.Filled.Done,
-                    contentDescription = "Done icon",
-                    modifier = Modifier.size(FilterChipDefaults.IconSize)
-                )
-            }
-        } else {
-            null
-        },
-    )
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun ItemPickPage(config: StatisticsConfig) {
-    val displayItems = remember { config.items }
-    LaunchedEffect("only once") {
-        SheetViewModel.itemNamesInSheet.forEach { item ->
-            val target = displayItems.find { it.first == item }
-            if (target == null) {
-                // item not show, make it unselected
-                displayItems.add(Pair(item, false))
-            }
-        }
-    }
-    FlowRow(modifier = Modifier.padding(10.dp).wrapContentSize()) {
-        SelectAllChip(
-            selected = displayItems.all { it.second },
-            onSelectChange = { selectAll ->
-                for (i in displayItems.indices) {
-                    displayItems[i] = Pair(displayItems[i].first, selectAll)
-                }
-            }
-        )
-        for (item in displayItems) {
-            ItemChip(name = item.first, selected = item.second, onSelectChange = {
-                val index = displayItems.indexOf(item)
-                displayItems[index] = Pair(item.first, !item.second)
-            })
-        }
-    }
-}
+//@OptIn(ExperimentalLayoutApi::class)
+//@Composable
+//fun ItemPickPage(config: StatisticsConfig) {
+//    val displayItems = remember { config.items }
+//    LaunchedEffect("only once") {
+//        SheetViewModel.itemNamesInSheet.forEach { item ->
+//            val target = displayItems.find { it.first == item }
+//            if (target == null) {
+//                // item not show, make it unselected
+//                displayItems.add(Pair(item, false))
+//            }
+//        }
+//    }
+//    FlowRow(modifier = Modifier.padding(10.dp).wrapContentSize()) {
+//        SelectAllChip(
+//            selected = displayItems.all { it.second },
+//            onSelectChange = { selectAll ->
+//                for (i in displayItems.indices) {
+//                    displayItems[i] = Pair(displayItems[i].first, selectAll)
+//                }
+//            }
+//        )
+//        for (item in displayItems) {
+//            SelectChip(name = item.first, selected = item.second, onSelectChange = {
+//                val index = displayItems.indexOf(item)
+//                displayItems[index] = Pair(item.first, !item.second)
+//            })
+//        }
+//    }
+//}
 
 @Composable
-fun ItemChip(name: String, selected: Boolean, onSelectChange: (Boolean) -> Unit) {
+fun SelectChip(name: String, selected: Boolean, onSelectChange: (Boolean) -> Unit) {
     FilterChip(
         modifier = Modifier.padding(5.dp),
         onClick = {
@@ -359,10 +350,9 @@ fun SelectAllChip(selected: Boolean, onSelectChange: (Boolean) -> Unit) {
 }
 
 @Composable
-fun ResultPage(result: StatisticsConfig.Result) {
+fun ResultPage(config: StatisticsConfig) {
 
-    // map from day of month to total price of items
-    val priceMap = remember { mutableStateMapOf<Int, Double>() }
+    val result = config.result
 
     LaunchedEffect("only once") {
         // sheets that fit date and customer
@@ -376,7 +366,7 @@ fun ResultPage(result: StatisticsConfig.Result) {
             if (sheetCustomerName.isBlank()) {
                 return@filter false
             }
-            if (result.customers.find { it == sheetCustomerName } == null) {
+            if (result.customerName != sheetCustomerName) {
                 return@filter false
             }
             return@filter true
@@ -400,24 +390,30 @@ fun ResultPage(result: StatisticsConfig.Result) {
             val sheetOfCurrDay = sheetsOfCurrDay.first()
             var totalPrice = 0.0
             for (item in sheetOfCurrDay.deliverItems) {
-                if (!item.valid || result.items.find { it == item.name } == null) {
+                if (!item.valid) {
                     continue
                 }
                 totalPrice += item.totalPrice
             }
             if (totalPrice > 0.0) {
-                priceMap[day] = totalPrice
+                config.priceMap[day] = totalPrice
             }
         }
 
     }
 
+    DisposableEffect("only once") {
+        onDispose {
+            config.priceMap.clear()
+        }
+    }
+
     LazyColumn {
-        items(priceMap.toList()) { (day, price) ->
+        items(config.priceMap.toList()) { (day, price) ->
             Text(text = "${result.year}年${result.month + 1}月${day}日: $price")
         }
         item {
-            priceMap.values.sumTotalPrice().takeIf { it > 0.0 }?.let {
+            config.priceMap.values.sumTotalPrice().takeIf { it > 0.0 }?.let {
                 Text(text = "总计: $it")
             }
         }
